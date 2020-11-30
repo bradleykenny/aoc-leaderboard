7fe2b48399273df89bc56e7c2c1b3d5ffdacc805
bradleykenny <bkenny283@gmail.com>
2020-11-30 18:24:12 +1100

    More for server.

--- server/app.js
+++ server/app.js
@@ -13,3 +13,20 @@ app.use(
 );
 app.use(bodyParser.json());
 app.use(express.static("../client/build"));
+
+app.set("port", process.env.PORT || 5000);
+
+// Test route to ensure up and running
+app.get("/api/ping", (req, res) => {
+	return res.send("pong");
+});
+
+// Catch all to avoid "cannot find /*" error
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "../../client/build/index.html"));
+});
+
+// Listening...
+app.listen(app.get("port"), () => {
+	console.log(`Server running on port ${app.get("port")}.`);
+}).on("error", (e) => console.error(e));
