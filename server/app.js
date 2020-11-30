63290869d29cbf8e0f788d296f02f7f605ae9f08
bradleykenny <bkenny283@gmail.com>
2020-11-30 18:22:34 +1100

    Renamed src/ to client/.

--- server/app.js
+++ server/app.js
@@ -1,1 +1,15 @@
 const express = require("express");
+const cors = require("cors");
+
+let bodyParser = require("body-parser");
+const app = express();
+
+// App configuration
+app.use(cors());
+app.use(
+	bodyParser.urlencoded({
+		extended: true,
+	})
+);
+app.use(bodyParser.json());
+app.use(express.static("../client/build"));
